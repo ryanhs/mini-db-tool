52ea533a68d658ac94fceb6eb4f3302f39d5eabc
ryan hs <mr.ryansilalahi@gmail.com>
2018-05-27 21:53:17 +0700

    example for db with eloquent

--- example-db.php
+++ example-db.php
@@ -4,10 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/bootstrap.php';
 
 use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
 /*
  * example DB
  */
+
+class Todo extends Eloquent {}
+class School extends Eloquent { public $timestamps = false; }
+
 echo env('DB_DATABASE');
 var_dump(DB::table('schools')->limit(2)->get());
 var_dump(DB::select('SHOW TABLES;'));
