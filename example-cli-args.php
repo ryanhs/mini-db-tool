481e618c8cece83a1b8016a91d411740eb217af4
ryan hs <mr.ryansilalahi@gmail.com>
2018-06-09 22:59:10 +0700

    cli agrs using fostam/getopts

--- example-cli-args.php
+++ example-cli-args.php
@@ -9,6 +9,8 @@ use PHPHtmlParser\Dom;
 
 $getopts = new Fostam\GetOpts\Handler();
 $getopts->addOption('debug')->long('debug')->description('show some logs');
+$getopts->addOption('truncate')->long('truncate')->description('truncate all filtered');
+$getopts->addOption('type')->argument('type')->long('type')->description('example --type=zonasi,prestasi');
 $getopts->parse();
 
 dd($getopts->get());
